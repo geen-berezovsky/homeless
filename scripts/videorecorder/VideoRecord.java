2290409014c073b1bcaf970fb2f755624ed88b6f
Maxim Grigorov <Mr.Maxim.Grigorov@gmail.com>
2016-02-13 14:54:44 +0300

    Trying to fix video recording

--- scripts/videorecorder/VideoRecord.java
+++ scripts/videorecorder/VideoRecord.java
@@ -1,6 +1,12 @@
 package ru.homeless.tests;
 
-import java.awt.*;  
+import java.awt.*;
+
+import org.monte.media.Format;
+import org.monte.media.FormatKeys;
+import org.monte.media.math.Rational;
+import static org.monte.media.AudioFormatKeys.*;
+import static org.monte.media.VideoFormatKeys.*;
 import org.monte.screenrecorder.ScreenRecorder;
 
 public class VideoRecord {  
@@ -11,7 +17,16 @@ public class VideoRecord {
                 .getLocalGraphicsEnvironment().getDefaultScreenDevice()
                 .getDefaultConfiguration();
 
-        screenRecorder = new ScreenRecorder(gc);
+        screenRecorder = new ScreenRecorder(gc,
+                new Format(MediaTypeKey, FormatKeys.MediaType.FILE, MimeTypeKey, MIME_AVI),
+                new Format(MediaTypeKey, FormatKeys.MediaType.VIDEO, EncodingKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
+                        CompressorNameKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
+                        DepthKey, (int)24, FrameRateKey, Rational.valueOf(15),
+                        QualityKey, 1.0f,
+                        KeyFrameIntervalKey, (int) (15 * 60)),
+                new Format(MediaTypeKey, FormatKeys.MediaType.VIDEO, EncodingKey,"black",
+                        FrameRateKey, Rational.valueOf(30)),
+                null);
         screenRecorder.start();
     }
 
