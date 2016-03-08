3c84c2e434552069c067abb1bee7663b6a687a59
Maxim Grigorov <Mr.Maxim.Grigorov@gmail.com>
2016-03-08 13:35:46 +0300

    Updated patch for initial dump & DEMO stand

--- db/patch.sql
+++ db/patch.sql
@@ -1,4 +1,6 @@
 select 1
-ALTER TABLE `homeless`.`ServContract`
-CHANGE COLUMN `commentResult` `commentResult` TEXT NULL DEFAULT NULL ;
+ALTER TABLE `homeless`.`ServContract` CHANGE COLUMN `commentResult` `commentResult` TEXT NULL DEFAULT NULL ;
+ALTER TABLE `homeless`.`ShelterHistory` ADD COLUMN `comments` MEDIUMTEXT NULL AFTER `servContract`;
+ALTER TABLE `homeless`.`ZAGSRequestDocumentRegistry` CHANGE COLUMN `name` `respAddress` TEXT NULL DEFAULT NULL ;
+
 
