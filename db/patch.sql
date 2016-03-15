4ed46875eed3457d3265fe532b4a0a1d33f12b93
Maxim Grigorov <Mr.Maxim.Grigorov@gmail.com>
2016-03-15 23:05:59 +0300

    Missing semicolon

--- db/patch.sql
+++ db/patch.sql
@@ -1,4 +1,4 @@
-select 1
+select 1 ;
 ALTER TABLE `homeless`.`ServContract` CHANGE COLUMN `commentResult` `commentResult` TEXT NULL DEFAULT NULL ;
 ALTER TABLE `homeless`.`ShelterHistory` ADD COLUMN `comments` MEDIUMTEXT NULL AFTER `servContract`;
 ALTER TABLE `homeless`.`ZAGSRequestDocumentRegistry` CHANGE COLUMN `name` `respAddress` TEXT NULL DEFAULT NULL ;
