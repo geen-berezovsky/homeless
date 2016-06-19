5951618ddec3a0d503330d00e9cd41d2e3437410
Maxim Grigorov <Mr.Maxim.Grigorov@gmail.com>
2016-06-20 00:52:13 +0300

    HS-58

--- src/main/java/ru/homeless/entities/ServicesType.java
+++ src/main/java/ru/homeless/entities/ServicesType.java
@@ -8,9 +8,13 @@ import javax.persistence.*;
 
 @Entity
 public class ServicesType implements Serializable {
-	private static final long serialVersionUID = 1L;
-	private int id;
-	private String caption;
+	
+    private static final long serialVersionUID = 2L;
+    private int id;
+    private String caption;
+    private Boolean money;
+    private Boolean document;
+    private int sort_order;
 
     @Basic
     @Column(columnDefinition = "BIT")
@@ -21,37 +25,45 @@ public class ServicesType implements Serializable {
     public void setMoney(Boolean money) {
         this.money = money;
     }
+	
+    public ServicesType() {
 
-    private Boolean money;
+    }
 
-    private Boolean document;
-	
-	public ServicesType() {
-		
-	}
-	
-	public ServicesType(String caption) {
-		setCaption(caption);
-	}
-	
-	@Id
-	@GeneratedValue(strategy = IDENTITY)
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getCaption() {
-		return caption;
-	}
-	public void setCaption(String caption) {
-		this.caption = caption;
-	}
-	
-	public String toString() {
-		return caption;
-	}
+    public ServicesType(String caption) {
+        setCaption(caption);
+    }
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public void setSort_order(int sort_order) {
+        this.sort_order = sort_order;
+    }
+
+    public int getSort_order() {
+        return sort_order;
+    }
+
+    @Override
+    public String toString() {
+        return caption;
+    }
 
     @Basic
     @Column(columnDefinition = "BIT")
@@ -63,5 +75,22 @@ public class ServicesType implements Serializable {
         this.document = document;
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (! (o instanceof ServicesType)) {
+            return false;
+        }
+        ServicesType d = (ServicesType) o;
+        return this.getId() == d.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + this.id;
+        return hash;
+    }
 
 }
